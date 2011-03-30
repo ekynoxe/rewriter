4e347e43b50625d2a5f98c41771f42221a938cdd
Mathieu Davy <mathieu.davy@betfair.com>
2011-03-30 14:42:33 +0100

    adding default login functionality, stuff needs to be cleaned, specially the requirement for error messages GEM if it's there

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_filter :require_user
+  
   def index
     @shared_url = SharedUrl.new
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110211142759) do
+ActiveRecord::Schema.define(:version => 20110330124733) do
 
   create_table "shared_urls", :force => true do |t|
     t.integer  "customer_id"
@@ -20,5 +20,19 @@ ActiveRecord::Schema.define(:version => 20110211142759) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "login",             :null => false
+    t.string   "email",             :null => false
+    t.string   "crypted_password",  :null => false
+    t.string   "password_salt",     :null => false
+    t.string   "persistence_token", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["login"], :name => "index_users_on_login", :unique => true
+  add_index "users", ["persistence_token"], :name => "index_users_on_persistence_token", :unique => true
 
 end
