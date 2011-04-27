b215574ad7f3c784efbb8bc20c6380bee49e3783
Mathieu Davy <mathieu.davy@betfair.com>
2011-04-27 09:42:59 +0100

    adding perishable token to user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110404135229) do
+ActiveRecord::Schema.define(:version => 20110427084036) do
 
   create_table "bookmarks", :force => true do |t|
     t.integer  "user_id"
@@ -36,10 +36,12 @@ ActiveRecord::Schema.define(:version => 20110404135229) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",             :default => false
+    t.string   "perishable_token",  :default => "",    :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
+  add_index "users", ["perishable_token"], :name => "index_users_on_perishable_token"
   add_index "users", ["persistence_token"], :name => "index_users_on_persistence_token", :unique => true
 
 end
