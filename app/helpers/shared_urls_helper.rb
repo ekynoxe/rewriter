46ca446f41ba8f50e153375d0306b2709f955d5b
Mathieu Davy <mathieu.davy@betfair.com>
2011-05-11 15:49:24 +0100

    formatting admin number of access per url

--- app/helpers/shared_urls_helper.rb
+++ app/helpers/shared_urls_helper.rb
@@ -1,2 +1,15 @@
 module SharedUrlsHelper
+  def format_shared_url_accessed(url)
+    if url.url_requests.size == 0
+      text = "never accessed"
+    elsif url.url_requests.size == 1
+      text = "accessed once"
+    elsif url.url_requests.size == 2
+      text = "accessed twice"
+    else
+      text = "accessed #{url.url_requests.size} times"
+    end
+    
+    text
+  end
 end
