dcec85e9ebb5a4ba8983001bc739cada17d81baf
Mathieu Davy <matt@ekynoxe.com>
2012-07-15 19:46:49 +0100

    Large redesign

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,6 +1,8 @@
 class UserSession < Authlogic::Session::Base
   find_by_login_method :find_by_login_or_email
   
+  generalize_credentials_error_messages "Login/password invaild"
+  
   def to_key
     new_record? ? nil : [ self.send(self.class.primary_key) ]
   end
